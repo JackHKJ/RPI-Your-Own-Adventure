66a89b5138a2fdd1071e27ae77538b12c1d77f8e
yangytjason2000 <yangyt.jason2015@outlook.com>
2021-04-04 23:25:25 -0700

    link to GUI
    link the new gatherer to the GUI

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
         User = Person("Guest")
     if USER_TYPE == UserTypeEnum.STUDENT:
         User = Person(GUI_thread.window.RIN)
-        User.add_skills_by_shortName(st, GUI_thread.window.gatherer.get_learned_courses())
+        User.add_skills_by_shortName(st, GUI_thread.window.gatherer.get_courses())
         st.pretty_print_partial_tree(User.get_skills(), save_fig=True)
         USER_GATHERER = GUI_thread.window.gatherer
 
