889b793f311c0b41fd2f621ebdc5fbf2ef041cfd
tn20000 <liangth.frank2015@outlook.com>
2021-04-05 13:51:36 +0800

    Changed login to public

--- basicClasses/sisscraper.py
+++ basicClasses/sisscraper.py
@@ -11,20 +11,21 @@ transcript_url = 'https://sis.rpi.edu/rss/bwskotrn.P_ViewTran'
 class SISscraper:
 
     def __init__(self, rin, password):
-        self.LOGIN_BASE_PARAMS = f"username={rin}&password={urllib.parse.quote(password)}&_eventId=submit"
-        self.LOGIN_RETRY_NUM = 3
+        self.__LOGIN_BASE_PARAMS = f"username={rin}&password={urllib.parse.quote(password)}&_eventId=submit"
+        self.__LOGIN_RETRY_NUM = 3
+        self.__session = requests.Session()
 
-    def __login(self, s):
+    def login(self):
         headers = {
             "Content-Type": "application/x-www-form-urlencoded",
         }
         login_url = "https://cas-auth-ent.rpi.edu/cas/login?service=https%3A%2F%2Fbannerapp04-bnrprd.server.rpi.edu%3A443%2Fssomanager%2Fc%2FSSB"
-        login_page = s.get(url=login_url)
+        login_page = self.__session.get(url=login_url)
         login_soup = BeautifulSoup(login_page.text.encode("utf8"), "html.parser")
         csrf = login_soup.find("input", attrs={"name": "execution"})["value"]
-        login_params = self.LOGIN_BASE_PARAMS + f"&execution={csrf}"
+        login_params = self.__LOGIN_BASE_PARAMS + f"&execution={csrf}"
 
-        response = s.request(
+        response = self.__session.request(
             "POST",
             login_url,
             headers=headers,
@@ -37,22 +38,21 @@ class SISscraper:
         return True, None
 
     def get_courses(self):
-        with requests.Session() as s:
-            for _ in range(self.LOGIN_RETRY_NUM):
-                result, data = self.__login(s)
-                if result:
-                    break
-                s.cookies.clear()
-                time.sleep(30)
-            else:
-                print(f"Failed to login to SIS {data}")
-                exit(1)
+        for _ in range(self.__LOGIN_RETRY_NUM):
+            result, data = self.login()
+            if result:
+                break
+            self.__session.cookies.clear()
+            time.sleep(30)
+        else:
+            print(f"Failed to login to SIS {data}")
+            return []
 
-            response = s.request('POST', transcript_url, data=f"levl=&tprt=UWEB")  
-            transcript_soup = BeautifulSoup(response.text.encode('utf8'), 'html.parser')
-            course_dept = transcript_soup.find_all('td', class_='dddefault', string=re.compile('^[A-Z]{4}$'))
-            course_num = transcript_soup.find_all('td', class_='dddefault', string=re.compile('^[0-9]{4}$'))
-            return [x.next_element + '-' + y.next_element for x, y in zip(course_dept, course_num)]
+        response = self.__session.request('POST', transcript_url, data=f"levl=&tprt=UWEB")  
+        transcript_soup = BeautifulSoup(response.text.encode('utf8'), 'html.parser')
+        course_dept = transcript_soup.find_all('td', class_='dddefault', string=re.compile('^[A-Z]{4}$'))
+        course_num = transcript_soup.find_all('td', class_='dddefault', string=re.compile('^[0-9]{4}$'))
+        return [x.next_element + '-' + y.next_element for x, y in zip(course_dept, course_num)]
 
 if __name__ == '__main__':
     load_dotenv()
